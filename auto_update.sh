01b160384949c57f764353309d08e035a0319973
CodeATA <cdwdhr@gmail.com>
2017-12-04 16:35:50 +0800

    Revert "2017-12-04 16:12:53 update"
    This reverts commit 624e9db0781d550f646f7909cdb06796d3e3dbb3.

--- auto_update.sh
+++ auto_update.sh
@@ -3,12 +3,8 @@
 rt_dir=$(pwd)
 spider_dir=$rt_dir/recharge
 
-#cd $spider_dir
-#scrapy crawl tb_recharge
-#mv a.out ..
-#cd $rt_dir
-#./update_csv.py
-
-timestamp=$(date +"%Y-%m-%d %H:%m:%S")
-git commit -a -m "$timestamp update"
-git push
+cd $spider_dir
+scrapy crawl tb_recharge
+mv a.out ..
+cd $rt_dir
+./update_csv.py
