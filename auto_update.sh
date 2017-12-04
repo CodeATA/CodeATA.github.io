624e9db0781d550f646f7909cdb06796d3e3dbb3
CDW <cdwdhr@gmail.com>
2017-12-04 16:32:54 +0800

    2017-12-04 16:12:53 update

--- auto_update.sh
+++ auto_update.sh
@@ -3,8 +3,12 @@
 rt_dir=$(pwd)
 spider_dir=$rt_dir/recharge
 
-cd $spider_dir
-scrapy crawl tb_recharge
-mv a.out ..
-cd $rt_dir
-./update_csv.py
+#cd $spider_dir
+#scrapy crawl tb_recharge
+#mv a.out ..
+#cd $rt_dir
+#./update_csv.py
+
+timestamp=$(date +"%Y-%m-%d %H:%m:%S")
+git commit -a -m "$timestamp update"
+git push
