e93ad6820076300394243d3d75b248cb4df87a2c
CDW <cdwdhr@gmail.com>
2017-12-05 17:25:01 +0800

    update

--- auto_update.sh
+++ auto_update.sh
@@ -3,10 +3,10 @@
 rt_dir=/home/cdw/CodeATA.github.io/
 spider_dir=$rt_dir/recharge
 
-cd $spider_dir
-PATH=$PATH:/usr/local/bin
-export PATH
-scrapy crawl tb_recharge
+#cd $spider_dir
+#PATH=$PATH:/usr/local/bin
+#export PATH
+#scrapy crawl tb_recharge
 mv $spider_dir/a.out $rt_dir
 $rt_dir/update_csv.py $rt_dir
 rm $rt_dir/a.out
