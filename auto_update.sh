bd83661f16fc632f3300db11fe6ae6c5b358154e
CDW <cdwdhr@gmail.com>
2017-12-05 17:00:01 +0800

    2017-12-05 17:00:01 update

--- auto_update.sh
+++ auto_update.sh
@@ -4,11 +4,13 @@ rt_dir=/home/cdw/CodeATA.github.io/
 spider_dir=$rt_dir/recharge
 
 cd $spider_dir
+PATH=$PATH:/usr/local/bin
+export PATH
 scrapy crawl tb_recharge
 mv $spider_dir/a.out $rt_dir
 $rt_dir/update_csv.py $rt_dir
 rm $rt_dir/a.out
 
-timestamp=$(date +"%Y-%m-%d %H:%m:%S")
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 git commit -a -m "$timestamp update"
 git push
