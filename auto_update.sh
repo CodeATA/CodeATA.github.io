cd457c02e8d671b99eac4e3f9ecad46d262ef573
root <cdwdhr@gmail.com>
2017-12-09 12:43:25 +0800

    save user data

--- auto_update.sh
+++ auto_update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rt_dir=/home/cdw/CodeATA.github.io/
+rt_dir=/root/CodeATA.github.io
 spider_dir=$rt_dir/recharge
 
-#cd $spider_dir
+cd $spider_dir
 #PATH=$PATH:/usr/local/bin
 #export PATH
-#scrapy crawl tb_recharge
+scrapy crawl tb_recharge
 mv $spider_dir/a.out $rt_dir
 $rt_dir/update_csv.py $rt_dir
 rm $rt_dir/a.out
