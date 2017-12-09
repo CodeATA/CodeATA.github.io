3667df49067c9e01da31b38d1cb35331443fc000
root <cdwdhr@gmail.com>
2017-12-09 15:05:01 +0800

    2017-12-09 15:05:01 update

--- auto_update.sh
+++ auto_update.sh
@@ -3,14 +3,15 @@
 rt_dir=/root/CodeATA.github.io
 spider_dir=$rt_dir/recharge
 
-cd $spider_dir
+#cd $spider_dir
 #PATH=$PATH:/usr/local/bin
 #export PATH
-scrapy crawl tb_recharge
+#scrapy crawl tb_recharge
 mv $spider_dir/a.out $rt_dir
 $rt_dir/update_csv.py $rt_dir
 rm $rt_dir/a.out
 
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+cd $rt_dir
 git commit -a -m "$timestamp update"
 git push
